578fd10b7c2f55ff6385a469278c114c60604437
CodeBlooded2017 <mike.d.fazio@gmail.com>
2018-03-27 18:51:14 -0400

    initial commit

---
+++ BluesBBQ/DAL/MenuContext.cs
@@ -0,0 +1,14 @@
+﻿using BluesBBQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BluesBBQ.DAL
+{
+    public class MenuContext : DbContext
+    {
+        public DbSet<MenuItem> MenuItems { get; set; }
+    }
+}
---
+++ BluesBBQ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BluesBBQ.MvcApplication" Language="C#" %>
